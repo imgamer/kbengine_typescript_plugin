4bcf6ca5c5ace8cfbda1c9daa6f108f913a39d15
shufengw <imgamer@163.com>
2018-06-15 15:24:18 +0800

    KBEMath

---
+++ kbengine/KBEMath.ts
@@ -0,0 +1,78 @@
+
+
+export const FLOAT_MAX_VALUE = 3.402823466e+38;
+
+export function Clamp(value: number, min: number, max: number): number
+{
+    if(min > max)
+    {
+        let mid = min;
+        min = max;
+        max = mid;
+    }
+
+    return value < min ? min : value > max? max: value;
+}
+
+export function Int82Angle(value: number, half: boolean): number
+{
+    return value * (Math.PI / (half ? 254.0 : 128.0));
+}
+
+export function Angle2Int8(value: number, half: boolean): number
+{
+    var angle = 0;
+    if(!half)
+    {
+        angle = Math.floor((value * 128.0) / Math.PI + 0.5);
+    }
+    else
+    {
+        angle = Clamp(Math.floor( (value * 254.0) / Math.PI + 0.5), -128.0, 127.0);
+    }
+
+    return angle;
+}
+
+export class Vector2
+{
+    x: number;
+    y: number;
+
+    constructor(x: number, y:number)
+    {
+        this.x = x;
+        this.y = y;
+    }
+
+    Distance(pos: Vector2)
+    {
+        let x = this.x - pos.x;
+        let y = this.y - pos.y;
+
+        return Math.sqrt(x * x + y * y);
+    }
+}
+
+export class Vector3
+{
+    x: number;
+    y: number;
+    z: number;
+
+    constructor(x: number, y:number, z:number)
+    {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    Distance(pos: Vector3)
+    {
+        let x = this.x - pos.x;
+        let y = this.y - pos.y;
+        let z = this.z - pos.z;
+
+        return Math.sqrt(x * x + y * y + z * z);
+    }
+}
