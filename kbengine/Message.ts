8a5586877c913d40b41fccd8fbc32714a2744a69
shufengw <imgamer@163.com>
2018-06-21 11:09:40 +0800

    Message封装

--- kbengine/Message.ts
+++ kbengine/Message.ts
@@ -1,20 +1,70 @@
 import KBEDebug from "./KBEDebug";
+import * as DataTypes from "./DataTypes";
+import MemoryStream from "./MemoryStream";
 
 export default class Message
 {
-    static messages: {[key:string]: Message};
+    static messages = {};
+    static clientMassges = {};
 
     id: number;
     name: string;
     length: number;
     argsType: number;
+    args: Array<DataTypes.DATATYPE_BASE> = new Array<DataTypes.DATATYPE_BASE>();
+    handler: Function = null;
 
-    constructor()
+    constructor(id: number, name: string, length: number, argstype: number, args: Array<number>, handler: Function)
     {
-        for(let key in Message.messages)
+        this.id = id;
+        this.name = name;
+        this.length = length;
+        this.argsType = argstype;
+
+        for(let argType of args)
         {
-            KBEDebug.WARNING_MSG("Message::constructor:let key(%s) in Message.messages............value(%s)", key, Message.messages[key]);
+            this.args.push(DataTypes.idToDatatype[argType]);
         }
+
+        this.handler = handler;
     }
 
+    private CreateFromStream(stream: MemoryStream)
+    {
+        if(this.args.length <= 0)
+            return stream;
+
+        let result = [];
+        for(let item of this.args)
+        {
+            result.push(item.CreateFromStream(stream));
+        }
+
+        return result;
+    }
+
+    HandleMessage(stream: MemoryStream): void
+    {
+        if(this.handler === null)
+        {
+            KBEDebug.ERROR_MSG("KBEngine.Message::handleMessage: interface(" + this.name + "/" + this.id + ") no implement!");
+            return;
+        }
+
+        if(this.args.length === 0)
+        {
+            if(this.argsType < 0)
+            {
+                this.handler(stream);
+            }
+            else
+            {
+                this.handler();
+            }
+        }
+        else
+        {
+            //this.handler.apply(KBEngine.app, this.CreateFromStream(stream));
+        }
+    }
 }
